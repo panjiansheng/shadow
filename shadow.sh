4b9bc0cd994311244edabe696acd282e929e7681
panjiansheng <bk20112370@my.swjtu.edu.cn>
2018-01-20 16:14:16 +0800

    Update shadow.sh

--- shadow.sh
+++ shadow.sh
@@ -3,4 +3,4 @@ apt install -y python-pip python-setuptools;
 pip install --upgrade pip;
 pip install shadowsocks;
 /usr/local/bin/ssserver -p 8388 -k 12345678 -m aes-256-cfb --user nobody --workers 2 -d start;
-sed -i '1i\/usr/local/bin/ssserver -p 8388 -k 12345678 -m aes-256-cfb --user nobody --workers 2 -d start' >> /etc/rc.local
+sed -i '1i\/usr/local/bin/ssserver -p 8388 -k 12345678 -m aes-256-cfb --user nobody --workers 2 -d start' /etc/rc.local
